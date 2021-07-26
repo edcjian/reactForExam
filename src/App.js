25d8d6a67655e075e22b1a9456fbe30c6fe3c7d1
edcjian <2015563489@qq.com>
2021-07-26 11:08:52 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {MyRoute, Tree} from "./component/Routers";
+import 'antd/dist/antd.css';
+
+const data = [
+    { name: 'src', children: [
+            { name: 'css', children: [] },
+            { name: 'js', children: [
+                    { name: 'utils', children: [] },
+                    { name: 'components', children: [] }
+                ]}
+        ]},
+    { name: 'public', children: [
+            { name: 'images', children: [] }
+        ]}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MyRoute/>
+        </div>
+    );
 }
 
 export default App;
